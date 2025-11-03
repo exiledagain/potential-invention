72fabbea55dfcd9dfe7ebd54b1117ad7d15e37ab
exiledagain <98454299+exiledagain@users.noreply.github.com>
2025-11-03 00:04:33 -0500

    Update NemesisRequest.js

--- ctrl-server/common/NemesisRequest.js
+++ ctrl-server/common/NemesisRequest.js
@@ -1,20 +1,16 @@
 const GenericRequest = require('./GenericRequest')
 
 async function NemesisRequest (data) {
-  try {
-    const ret = await GenericRequest(2, data)
-    if (typeof ret !== 'object') {
-      return ret
-    }
-    if (ret.length !== 8) {
-      return 'Last Epoch is unavailable or your data is wrong.'
-    }
-    const passes = await ret.stream.readInt32()
-    const amount = await ret.stream.readInt32()
-    return { passes, amount }
-  } catch (e) {
-    console.error(e)
+  const ret = await GenericRequest(2, data)
+  if (typeof ret !== 'object') {
+    return ret
   }
+  if (ret.length !== 8) {
+    return 'Last Epoch is unavailable or your data is wrong.'
+  }
+  const passes = await ret.stream.readInt32()
+  const amount = await ret.stream.readInt32()
+  return { passes, amount }
 }
 
 module.exports = NemesisRequest
